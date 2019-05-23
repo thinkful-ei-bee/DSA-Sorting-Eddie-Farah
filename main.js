e54409d81ab0df76dae6994097a6e38f6059a71f
Farah Student <farah.gus@hotmail.com>
2019-05-23 14:18:29 -0400

    finished implementing merge sort

--- main.js
+++ main.js
@@ -84,22 +84,49 @@ function swap(array,i,j){
 }
 
 const tempArr = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
+
 // console.log(qSort(tempArr));
 
 
 // Implementing merge sort
 function msort(array) {
-    const middle = Math.floor(array[array.length / 2]);
+    if (array.length <= 1) {
+        return array;
+    }
 
-    const left = array.slice(0, middle);
-    const right = array.slice(middle, array.length-1)
+    let middle = Math.floor(array.length / 2);
+
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length)
 
     left = msort(left);
     right = msort(right);
 
-    merge(left, right, array);
+    return merge(array, left, right);
 }
 
 function merge(array, left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0; // right
+    let k = 0;
 
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            array[k++] = left[leftIndex++]
+        } else {
+            array[k++] = right[rightIndex++]
+        }
+    }
+
+    for (let i = leftIndex; i < left.length; i++ ){
+        array[k++] = left[i];
+    }
+    for (let j = rightIndex; j < right.length; j++) {
+        array[k++] = right[j];
+    }
+
+    return array;
 }
+
+
+console.log(msort(tempArr));
